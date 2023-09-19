a5a0a69358ce3b55b495f2d209689613ce01d1cc
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-09-19 11:35:32 +0700

    Update auxiliary_processing.py

--- utils/auxiliary_processing.py
+++ utils/auxiliary_processing.py
@@ -8,13 +8,25 @@ def random_range(a=0, b=1):
 
 def change_color_space(image, current_space="BGR", to_space="BGR"):
     if not ((current_space.lower() in {'bgr', 'rgb', 'hsv', 'gray'}) 
-             and (to_space.lower() in {'bgr', 'rgb', 'hsv', 'gray'})):
+            and (to_space.lower() in {'bgr', 'rgb', 'hsv', 'gray'})):
         raise NotImplementedError
     if current_space.lower() != 'gray' or (image.shape[-1] != 1 and len(image.shape) > 2):
         if current_space.lower() == 'bgr' and to_space.lower() == 'rgb':
             return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        if current_space.lower() == 'rgb' and to_space.lower() == 'bgr':
+        elif current_space.lower() == 'rgb' and to_space.lower() == 'bgr':
             return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         elif current_space.lower() == 'bgr' and to_space.lower() == 'hsv':
             return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        elif current_space.lower() == 'hsv' and to_space.lower() == 'bgr':
+            return cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+        elif current_space.lower() == 'rgb' and to_space.lower() == 'hsv':
+            return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        elif current_space.lower() == 'hsv' and to_space.lower() == 'rgb':
+            return cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+        elif current_space.lower() == 'bgr' and to_space.lower() == 'gray':
+            return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        elif current_space.lower() == 'rgb' and to_space.lower() == 'gray':
+            return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        elif current_space.lower() == 'hsv' and to_space.lower() == 'gray':
+            return cv2.cvtColor(image, cv2.COLOR_HSV2GRAY)
     return image
