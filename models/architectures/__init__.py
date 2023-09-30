d00d33cc5de625af9374f2b4c0efdc4bc6ebefe4
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-09-30 22:49:09 +0700

    Create __init__.py

---
+++ models/architectures/__init__.py
@@ -0,0 +1,14 @@
+from .vgg import VGG, VGG11, VGG13, VGG16, VGG19
+from .resnet import ResNetA, ResNetB, ResNet18, ResNet18_backbone, ResNet34, ResNet34_backbone, ResNet50, ResNet50_backbone,  ResNet101, ResNet101_backbone, ResNet152, ResNet152_backbone
+from .densenet import DenseNet, DenseNet121, DenseNet121_backbone, DenseNet169, DenseNet169_backbone, DenseNet201, DenseNet201_backbone, DenseNet264, DenseNet264_backbone
+from .efficientnet import EfficientNet, EfficientNetB0, EfficientNetB1, EfficientNetB2, EfficientNetB3, EfficientNetB4, EfficientNetB5, EfficientNetB6, EfficientNetB7
+from .res2net import Res2Net, Res2Net50, ResNet50_backbone, Res2Net101, ResNet101_backbone, Res2Net152, ResNet152_backbone
+from .xception import Xception
+from .ddrnet import DDRNet23_slim, DDRNet23, DDRNet39
+from .bit import ResnetV2, BiT_S_R50x1, BiT_S_R50x3, BiT_M_R50x1, BiT_M_R50x3, BiT_S_R101x1, BiT_S_R101x3, BiT_M_R101x1, BiT_M_R101x3, BiT_S_R152x4, BiT_M_R152x4
+from .convnext import ConvNext, ConvNextT, ConvNextT_backbone, ConvNextS, ConvNextS_backbone, ConvNextB, ConvNextB_backbone, ConvNextL, ConvNextL_backbone, ConvNextXL, ConvNextXL_backbone
+
+from .mlp_mixer import MLPMixer, MLPMixer_S16, MLPMixer_S32, MLPMixer_B16, MLPMixer_B32, MLPMixer_L16, MLPMixer_L32, MLPMixer_H14
+from .vit import ViT, ViTBase16, ViTBase32, ViTLarge16, ViTLarge32, ViTHuge16, ViTHuge32
+from .deit import DeiT
+from .swin import Swin, SwinT, SwinS, SwinB, SwinL
