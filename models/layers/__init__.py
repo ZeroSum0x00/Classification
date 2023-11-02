642c6f873782c0b7b1d5c5f692c18a8f4e95d20f
ZeroSum0x00 <nguyenichthanhtu0509@gmail.com>
2023-11-02 10:54:09 +0700

    add activations

--- models/layers/__init__.py
+++ models/layers/__init__.py
@@ -1,4 +1,9 @@
-from .activation import get_activation_from_name, ReLU6, Mish
+from .activation import (get_activation_from_name,
+                         Mixture, HardTanh, HardSwish,
+                         ReLU6, AReLU, FReLU, 
+                         Mish, MemoryEfficientMish, SiLU,
+                         GELUQuick, GELULinear, 
+                         AconC, MetaAconC, ELSA)
 from .normalization import get_normalizer_from_name
 from .stochastic_depth import StochasticDepth, StochasticDepth2
 from .single_stochastic_depth import DropPath
