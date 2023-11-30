146b6586549ccf0f3f04a347f5f59ff64c5a34af
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-11-30 14:21:43 +0700

    Update linear_layer.py

--- models/layers/linear_layer.py
+++ models/layers/linear_layer.py
@@ -3,6 +3,9 @@ import tensorflow.keras.backend as K
 
 
 class LinearLayer(tf.keras.layers.Layer):
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        
     def call(self, inputs, training=False):
         return inputs
