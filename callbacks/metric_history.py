3171a05afc3c5842262568ec8be7f2ae30e949a6
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-12-04 10:22:19 +0700

    Update metric_history.py

--- callbacks/metric_history.py
+++ callbacks/metric_history.py
@@ -59,67 +59,6 @@ class MetricHistory(tf.keras.callbacks.Callback):
             plt.cla()
             plt.close("all")
             
-import os
-import cv2
-import shutil
-import numpy as np
-from tqdm import tqdm
-import tensorflow as tf
-import scipy.signal
-from utils.logger import logger
-
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-
-
-class MetricHistory(tf.keras.callbacks.Callback):
-    def __init__(self,
-                 result_path=None, 
-                 save_best=False,
-                 min_ratio=0.2):
-        super(MetricHistory, self).__init__()
-        self.result_path            = result_path
-        self.save_best              = save_best
-        self.min_ratio              = min_ratio
-        self.metric_infomation = {}
-
-    def on_epoch_end(self, epoch, logs={}):
-        for metric in self.model.list_metrics:
-            metric_name = metric.name
-            if metric_name not in self.metric_infomation:
-                self.metric_infomation[metric_name] = {}
-                self.metric_infomation[metric_name]['train_object'] = []
-                self.metric_infomation[metric_name]['valid_object'] = []
-                self.metric_infomation[metric_name]['train_value'] = 0.0
-                self.metric_infomation[metric_name]['valid_value'] = 0.0
-
-            train_value = logs.get(metric_name)
-            valid_value = logs.get('val_' + metric_name)
-            self.metric_infomation[metric_name]['train_object'].append(train_value)
-            self.metric_infomation[metric_name]['valid_object'].append(valid_value)
-            
-            with open(os.path.join(self.result_path, f"train_{metric_name}.txt"), 'a') as f:
-                f.write(f"Train {metric_name} in epoch {epoch + 1}: {str(train_value)}")
-                f.write("\n")
-            with open(os.path.join(self.result_path, f"val_{metric_name}.txt"), 'a') as f:
-                f.write(f"Valid {metric_name} in epoch {epoch + 1}: {str(valid_value)}")
-                f.write("\n")
-                
-            iters = range(len(self.metric_infomation[metric_name]['train_object']))
-    
-            plt.figure()
-            plt.plot(iters, self.metric_infomation[metric_name]['train_object'], 'red', linewidth=2, label=f'train {metric_name.replace("-", " ")}')
-            plt.plot(iters, self.metric_infomation[metric_name]['valid_object'], 'coral', linewidth=2, label=f'valid {metric_name.replace("-", " ")}')
-            plt.grid(True)
-            plt.xlabel('Epoch')
-            plt.ylabel(metric_name.replace("-", " ").title())
-            plt.title(f'A {metric_name.replace("-", " ").title()} Curve')
-            plt.legend(loc="lower right")
-            plt.savefig(os.path.join(self.result_path, f"epoch_{metric_name}.png"))
-            plt.cla()
-            plt.close("all")
-            
             if self.save_best:
                 print('')
                 if train_value > self.metric_infomation[metric_name]['train_value'] and train_value > self.min_ratio:
