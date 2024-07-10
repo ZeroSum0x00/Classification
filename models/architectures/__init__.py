1510382875820202a920983a3d6b84c445bf3376
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2024-07-10 16:32:30 +0700

    Update __init__.py

--- models/architectures/__init__.py
+++ models/architectures/__init__.py
@@ -1,7 +1,3 @@
-# train-test processing model
-from .classification import CLS
-
-
 # convolutional base architectures
 from .architectures import (AlexNet,
                             VGG, VGG11, VGG11_backbone, VGG13, VGG13_backbone, VGG16, VGG16_backbone, VGG19, VGG19_backbone,
