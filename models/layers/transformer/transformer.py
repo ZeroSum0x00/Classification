1c35cbc7f76296b50149110e0d19522c3d04d377
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2025-08-20 12:26:56 +0700

    Update transformer.py

--- models/layers/transformer/transformer.py
+++ models/layers/transformer/transformer.py
@@ -9,8 +9,8 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
 
     def __init__(
         self,
-        attention_block=None,
-        mlp_block=None,
+        attn_block=None,
+        ffn_block=None,
         activation=None,
         normalizer="layer-norm",
         norm_eps=1e-6,
@@ -18,8 +18,8 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
         *args, **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.attention_block = attention_block
-        self.mlp_block = mlp_block
+        self.attn_block = attn_block
+        self.ffn_block = ffn_block
         self.activation = activation
         self.normalizer = normalizer
         self.norm_eps = norm_eps
@@ -28,31 +28,31 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
     def build(self, input_shape):
         self.norm_layer1 = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
         self.norm_layer2 = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
-        self.dropout_layer1 = Dropout(rate=self.drop_rate)
-        self.dropout_layer2 = Dropout(rate=self.drop_rate)
+        self.norm_layer3 = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
         self.activ_layer = get_activation_from_name(self.activation)
+        self.dropout = Dropout(rate=self.drop_rate)
 
     def call(self, inputs, attn_mask=None, training=False, return_weight=False):
-        x = self.norm_layer1(inputs, training=training)
-        x, weights = self.attention_block(
-            x, attn_mask=attn_mask, training=training, return_weight=return_weight
+        hidden_state = self.norm_layer1(inputs, training=training)
+        attn_out, weights = self.attn_block(
+            hidden_state, attn_mask=attn_mask, training=training, return_weight=return_weight
         )
-        x = self.dropout_layer1(x, training=training)
-        x = x + inputs
+        hidden_state = hidden_state + attn_out
         
-        y = self.norm_layer2(x, training=training)
-        y = self.mlp_block(y, training=training)
-        y = self.dropout_layer2(y, training=training)
+        hidden_state = self.norm_layer2(hidden_state, training=training)
+        ffn_out = self.ffn_block(hidden_state, training=training)
+        hidden_state = hidden_state + ffn_out
         
-        o = x + y
-        o = self.activ_layer(o, training=training)
-        return o, weights
+        hidden_state = self.norm_layer3(hidden_state, training=training)
+        hidden_state = self.activ_layer(hidden_state, training=training)
+        hidden_state = self.dropout(hidden_state, training=training)
+        return hidden_state, weights
 
     def get_config(self):
         config = super().get_config()
         config.update({
-            "attention_block": self.attention_block,
-            "mlp_block": self.mlp_block,
+            "attn_block": self.attn_block,
+            "ffn_block": self.ffn_block,
             "activation": self.activation,
             "normalizer": self.normalizer,
             "norm_eps": self.norm_eps,
@@ -69,9 +69,9 @@ class TransformerDecoderBlock(tf.keras.layers.Layer):
 
     def __init__(
         self,
-        masked_attention_block=None,
-        cross_attention_block=None,
-        mlp_block=None,
+        masked_attn_block=None,
+        cross_attn_block=None,
+        ffn_block=None,
         activation=None,
         normalizer="layer-norm",
         norm_eps=1e-6,
@@ -79,55 +79,56 @@ class TransformerDecoderBlock(tf.keras.layers.Layer):
         *args, **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.masked_attention_block = masked_attention_block
-        self.cross_attention_block = cross_attention_block
-        self.mlp_block = mlp_block
+        self.masked_attn_block = masked_attn_block
+        self.cross_attn_block = cross_attn_block
+        self.ffn_block = ffn_block
         self.activation = activation
         self.normalizer = normalizer
         self.norm_eps = norm_eps
         self.drop_rate = drop_rate
     
     def build(self, input_shape):
+        if self.cross_attn_block is not None:
+            self.norm_layer_cross = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
+            
         self.norm_layer1 = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
         self.norm_layer2 = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
         self.norm_layer3 = get_normalizer_from_name(self.normalizer, epsilon=self.norm_eps)
-        self.dropout_layer1 = Dropout(rate=self.drop_rate)
-        self.dropout_layer2 = Dropout(rate=self.drop_rate)
-        self.dropout_layer3 = Dropout(rate=self.drop_rate)
         self.activ_layer = Activation(self.activation)
+        self.dropout = Dropout(self.drop_rate)
 
     def call(self, inputs, self_mask=None, cross_mask=None, training=False, return_weight=False):
         hidden_state, encoder_outputs = inputs
-        x = self.norm_layer1(hidden_state, training=training)
-        x, weights1 = self.masked_attention_block(
-            x, attn_mask=self_mask, training=training, return_weight=return_weight
+        hidden_state = self.norm_layer1(hidden_state, training=training)
+        masked_attn_out, weights1 = self.masked_attn_block(
+            hidden_state, attn_mask=self_mask, training=training, return_weight=return_weight
         )
-
-        x = self.dropout_layer1(x, training=training)
-        x = x + hidden_state
+        hidden_state = hidden_state + masked_attn_out
         
-        if self.cross_attention_block is not None:
-            x = self.norm_layer2(x, training=training)
-            x, weights2 = self.cross_attention_block(
-                [x, encoder_outputs], attn_mask=cross_mask, training=training, return_weight=return_weight
+        if self.cross_attn_block is not None:
+            hidden_state = self.norm_layer_cross(hidden_state, training=training)
+            cross_attn_out, weights2 = self.cross_attn_block(
+                [hidden_state, encoder_outputs], attn_mask=cross_mask, training=training, return_weight=return_weight
             )
+            hidden_state = hidden_state + cross_attn_out
         else:
             weights2 = tf.constant(0.0)
 
-        y = self.norm_layer3(x, training=training)
-        y = self.mlp_block(y, training=training)
-        y = self.dropout_layer2(y, training=training)
+        hidden_state = self.norm_layer2(hidden_state, training=training)
+        ffn_out = self.ffn_block(hidden_state, training=training)        
+        hidden_state = hidden_state + ffn_out
         
-        o = x + y
-        o = self.activ_layer(o, training=training)
-        return o, weights1, weights2
+        hidden_state = self.norm_layer3(hidden_state, training=training)
+        hidden_state = self.activ_layer(hidden_state, training=training)
+        hidden_state = self.dropout(hidden_state, training=training)
+        return hidden_state, weights1, weights2
 
     def get_config(self):
         config = super().get_config()
         config.update({
-            "masked_attention_block": self.masked_attention_block,
-            "cross_attention_block": self.cross_attention_block,
-            "mlp_block": self.mlp_block,
+            "masked_attn_block": self.masked_attn_block,
+            "cross_attn_block": self.cross_attn_block,
+            "ffn_block": self.ffn_block,
             "activation": self.activation,
             "normalizer": self.normalizer,
             "norm_eps": self.norm_eps,
